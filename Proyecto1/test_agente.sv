c2cb3ee54c4084071a0550fdba6989d2f7d0d914
Denisse <denissesolorzanoruiz@gmail.com>
2023-10-04 20:51:14 -0600

    Agente y tb_agente listo

--- Proyecto1/test_agente.sv
+++ Proyecto1/test_agente.sv
@@ -1,10 +1,10 @@
 // Code your testbench here
 // or browse Examples
 `timescale 1ns/1ps
-`include "fifo.sv"
-`include "interface_transactions.sv"
-`include "driver.sv"
-`include "checker.sv"
+//`include "fifo.sv"
+`include "interface_transaction.sv"
+//`include "driver.sv"
+//`include "checker.sv"
 `include "Library.sv"
 `include "agent.sv"
 
@@ -14,12 +14,6 @@ module tb_driver_dut;
   parameter deep_fifo = 8;
   parameter drvrs = 4;
   parameter bits = 1;
-  //reg                reset;
-  //reg                pndng  [bits-1:0][drvrs-1:0];
-  //reg [pckg_sz-1:0]  D_pop  [bits-1:0][drvrs-1:0];
-  //wire               push   [bits-1:0][drvrs-1:0];
-  //wire               pop    [bits-1:0][drvrs-1:0];
-  //wire [pckg_sz-1:0] D_push [bits-1:0][drvrs-1:0];
   bit [pckg_sz-1:0] dto_spec [drvrs-1: 0];
   
   tipo_trans tpo_spec;
@@ -29,18 +23,13 @@ module tb_driver_dut;
     forever #5 clk = ~clk;
   end
   
-  driver #(.pckg_sz(pckg_sz), .drvrs(drvrs), .deep_fifo(deep_fifo), .bits(bits)) driver_prueba;
-  
-  check #(.pckg_sz(pckg_sz), .drvrs(drvrs)) checker_prueba;
-  //bus_if #(.drvrs(drvrs), .pckg_sz(pckg_sz)) _if (.clk(clk));
   
   agent #(.width(pckg_sz), .depth(deep_fifo), .drvrs(drvrs)) agente_prueba;
   
-  trans_fifo_mbx agnt_drv_mbx;
-  trans_fifo_mbx drv_chkr_mbx;
+  trans_fifo_mbx agnt_drv_mbx_tb;
   trans_fifo_mbx tst_agnt_mbx;
   
-  trans_sb_mbx chkr_sb_mbx;
+
   
   trans_fifo #(.pckg_sz(pckg_sz), .drvrs(drvrs)) transaccion;
   
@@ -62,59 +51,29 @@ module tb_driver_dut;
                         );
   initial begin
     
-  	//reset= 1;
-  	//#10
-  	//reset = 0;
-    agnt_drv_mbx = new;
-    drv_chkr_mbx = new;
-    chkr_sb_mbx  = new;
-    driver_prueba  = new;
-    checker_prueba = new;
-    agente_prueba  = new;
-    
-   	driver_prueba.vif = vif;
-    
+    agnt_drv_mbx_tb = new();
+    agente_prueba  = new();
+    transaccion= new();
     // Instanciando el MB 
-    test_agent_mbx = new;
+    test_agent_mbx = new();
+    
     agente_prueba.test_agent_mbx = test_agent_mbx; // MB entre el TB y el agente la
     //Instruccion al mailbox del agente
     instr_agent = IDaleatorio;
     test_agent_mbx.put(instr_agent);
-    
-    
-    //transaccion = new;
-    //tpo_spec = escritura;
-    //transaccion.tipo = tpo_spec;
-    //transaccion.dato [0] =  16'b00000001_00000010;
-    //transaccion.dato [1] =  16'b00000010_00000011;
-    //transaccion.dato [2] =  16'b00000001_00000001;
-    //transaccion.retardo = 1;
-    //transaccion.print("Agente: transacción creada");
-    //agnt_drv_mbx.put(transaccion);
-    
-    
-    
-    //transaccion = new;
-    //transaccion.tipo = lectura;
-    //transaccion.retardo = 1;
-    //transaccion.print("Agente: transacción creada");
-    //agnt_drv_mbx.put(transaccion);
-    
-    
    
-    agente_prueba.agnt_drv_mbx  = agnt_drv_mbx;
-    driver_prueba.agnt_drv_mbx  = agnt_drv_mbx;
-    driver_prueba.drv_chkr_mbx = drv_chkr_mbx;
-    checker_prueba.drv_chkr_mbx = drv_chkr_mbx;
-    checker_prueba.chkr_sb_mbx = chkr_sb_mbx;
-    
+   
+    agente_prueba.agnt_drv_mbx = agnt_drv_mbx_tb;
+  	
     
     fork
-      driver_prueba.run();
-      driver_prueba.detec_push();
-      checker_prueba.run();
+      
       agente_prueba.run();
+      
     join_none
+    
+    agnt_drv_mbx.get(transaccion);
+    $display("Transaccion RECIBIDA Source= %d dato=%0h", transaccion.drvSource,transaccion.dato);
     
   end
   initial begin
