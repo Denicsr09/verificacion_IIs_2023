a013afdbdafe2ab844bdf0d74ad5e289fc87921c
CarlosQG181 <c.quiros@estudiantec.cr>
2023-10-25 21:20:08 -0600

    agregando

--- Proyecto2/csrc/filelist.cu
+++ Proyecto2/csrc/filelist.cu
@@ -5,12 +5,12 @@ ifeq ($(LDVERSION), 0)
 endif
 
 ARCHIVE_OBJS=
-ARCHIVE_OBJS += _1497051_archive_1.so
-_1497051_archive_1.so : archive.0/_1497051_archive_1.a
+ARCHIVE_OBJS += _2660390_archive_1.so
+_2660390_archive_1.so : archive.0/_2660390_archive_1.a
 	@$(AR) -s $<
-	@$(PIC_LD) -shared  -Bsymbolic $(LD_NORELAX_FLAG)  -o .//../salida.daidir//_1497051_archive_1.so --whole-archive $< --no-whole-archive
+	@$(PIC_LD) -shared  -Bsymbolic $(LD_NORELAX_FLAG)  -o .//../salida.daidir//_2660390_archive_1.so --whole-archive $< --no-whole-archive
 	@rm -f $@
-	@ln -sf .//../salida.daidir//_1497051_archive_1.so $@
+	@ln -sf .//../salida.daidir//_2660390_archive_1.so $@
 
 
 
