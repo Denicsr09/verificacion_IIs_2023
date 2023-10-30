ae256ffed4f053d8dec7ae81d2e769352e5c3997
Carlos Quiros <c.quiros@estudiantec.cr>
2023-10-29 23:27:11 -0600

    coment

--- Proyecto2/fifo_in.sv
+++ Proyecto2/fifo_in.sv
@@ -1,7 +1,8 @@
+//Clase para emular el comportamiento de fifo
 class fifo_in #(parameter ROWS=4,parameter COLUMS=4, parameter pckg_sz = 40, parameter deep_fifo = 4);
   virtual mesh_gnrtr_vif  #(.ROWS(ROWS), .COLUMS(COLUMS), .pckg_sz(pckg_sz), .fifo_depth(deep_fifo)) vif;
   
-	bit [pckg_sz - 1: 0] Din;
+	bit [pckg_sz - 1: 0] Din; 
 	bit [pckg_sz - 1: 0] Dout;
 	//bit push;
 	//bit popin;
@@ -25,11 +26,12 @@ class fifo_in #(parameter ROWS=4,parameter COLUMS=4, parameter pckg_sz = 40, par
     
     forever begin
       @(posedge vif.clk);
+      //Si la fifo tiene un dato activa la bandera de pending
       if(this.cola.size() == 0) begin
         this.vif.pndng_i_in[fifo_num]=0;
       end
       else  this.vif.pndng_i_in[fifo_num]=1;
-      
+      //Si la fifo detecta una señal de pop del DUT, saca el dato de esta
       if(this.vif.popin[fifo_num]) begin
         $display("salió un dato");
         this.vif.data_out_i_in[fifo_num]=this.cola.pop_front();
@@ -39,7 +41,7 @@ class fifo_in #(parameter ROWS=4,parameter COLUMS=4, parameter pckg_sz = 40, par
     end
     
   endtask
-  
+  //Task para meter datos a la fifo
   task fifo_push(bit[pckg_sz-1:0] dto);
     if(this.cola.size() == 0) begin
       this.Dout = dto;
