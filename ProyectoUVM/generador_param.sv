8a69f3811f34ddcdff432449725119bff2bc13dc
CarlosQG181 <c.quiros@estudiantec.cr>
2023-11-23 01:50:49 -0600

    sub

--- ProyectoUVM/generador_param.sv
+++ ProyectoUVM/generador_param.sv
@@ -3,19 +3,19 @@ module generador_param;
         integer f;
         rand int unsigned pckg_sz;
         rand int unsigned deep_fifo;
-        rand int unsigned num_transacciones;
+        rand int unsigned num_transaciones;
         int unsigned ROWS = 4;
         int unsigned COLUMS = 4;
         //Constraints para limitar los parametros
         constraint C0 {pckg_sz >= 35 ; pckg_sz < 50;}
         constraint C1 {deep_fifo >= 1 ; deep_fifo < 30;}
-        constraint C2 {num_transacciones >= 500; num_transacciones <= 1000;}
+        constraint C2 {num_transaciones >= 500; num_transaciones <= 1000;}
 
         function void printmacros;
             f = $fopen("macros2.sv","w");
-            $fdisplay(f, "`define pckg_size %0d", pckg_sz);
+            $fdisplay(f, "`define pckg_sz %0d", pckg_sz);
             $fdisplay(f, "`define deep_fifo %0d", deep_fifo );
-            $fdisplay(f, "`define num_transacciones %0d", num_transacciones );
+            $fdisplay(f, "`define num_transaciones  %0d", num_transaciones );
             $fdisplay(f, "`define ROWS %0d", ROWS );
             $fdisplay(f, "`define COLUMS %0d", COLUMS );
         endfunction
--- ProyectoUVM/macros2.sv
+++ ProyectoUVM/macros2.sv
@@ -1,5 +1,5 @@
-`define pckg_sz 40
-`define deep_fifo 4
-`define num_transaciones 1000
+`define pckg_sz 49
+`define deep_fifo 3
+`define num_transaciones  668
 `define ROWS 4
 `define COLUMS 4
